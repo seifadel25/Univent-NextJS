5902b1904d365ca7b2a49a7488d650417a10bb06
seifadel25 <62226196+seifadel25@users.noreply.github.com>
2024-05-06 14:48:39 +0300

    Update home.tsx

--- app/home.tsx
+++ app/home.tsx
@@ -3,9 +3,9 @@ import { HeroHighlight, Highlight } from "./components/hero-highlight";
 export default function HomePage() {
   return (
     <div className="">
-      <HeroHighlight className="text-4xl md:text-5xl lg:text-6xl text-center leading-loose">
+      <HeroHighlight className="text-4xl md:text-5xl lg:text-6xl text-center leading-loose text-text-light dark:text-text-dark">
         Univent: Where Quality Is Everything. Excellence in Every Detail,
-        <Highlight>Trust in Every System.</Highlight>
+        <Highlight className="leading-loose">Trust in Every System.</Highlight>
       </HeroHighlight>
     </div>
   );
