7628291a2997e97233d5a6b44f192192e3e89357
seifadel25 <62226196+seifadel25@users.noreply.github.com>
2024-05-09 13:24:56 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 const inter = Inter({ subsets: ["latin"] });
 import { Analytics } from "@vercel/analytics/react";
-import NavbarComp from "./components/navbar";
+import NavbarComp from "./components/Navbar";
 import Footer from "./components/Footer";
 export const metadata: Metadata = {
   title: "Univent",
