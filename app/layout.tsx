05699bab18f28622ebe5bfb747ec2bb3e77bc9d4
seifadel25 <62226196+seifadel25@users.noreply.github.com>
2024-06-04 00:17:38 +0300

    04.06.2024

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
   title: "Univent",
   description:
     "Delivering customized HVAC solutions for homes and businesses that need efficiency, reliability, and exceptional support",
+  icons: [],
 };
 function setInitialTheme() {
   return `
@@ -30,6 +31,36 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script dangerouslySetInnerHTML={{ __html: setInitialTheme() }} />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="android-chrome-192x192"
+          href="/android-chrome-192x192.png"
+          sizes="192x192"
+        />
+        <link
+          rel="android-chrome-512x512"
+          href="/android-chrome-512x512.png"
+          sizes="512x512"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={inter.className}>
         <div className="bg-white dark:bg-black">
